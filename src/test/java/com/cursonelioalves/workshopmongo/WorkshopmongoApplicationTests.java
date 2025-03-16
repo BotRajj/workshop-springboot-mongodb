c26d7bdc3bb7a1766d0ff2e1826a9bbbf5f622a7
Vitor S M <vitorsilva3579@gmail.com>
2025-03-16 14:40:00 -0300

    Projeto criado

---
+++ src/test/java/com/cursonelioalves/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursonelioalves.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
