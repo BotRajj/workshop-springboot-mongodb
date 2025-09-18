adbc0e0bb2c89581a865cc7e412f4c2303f97f22
Vitor Silva <vitorsilva3579@gmail.com>
2025-09-17 21:47:17 -0300

    Consulta com varios criterios

--- src/main/java/com/cursonelioalves/workshopmongo/dto/CommentDTO.java
+++ src/main/java/com/cursonelioalves/workshopmongo/dto/CommentDTO.java
@@ -1,9 +1,7 @@
 package com.cursonelioalves.workshopmongo.dto;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class CommentDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -11,8 +9,6 @@ public class CommentDTO implements Serializable{
 	private String text;
 	private Date date;
 	private AuthorDTO author;
-	
-	private List<CommentDTO> comments = new ArrayList<>();
 		
 	public CommentDTO(String text, Date date, AuthorDTO author) {
 		super();
@@ -44,13 +40,4 @@ public class CommentDTO implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
-
-	public List<CommentDTO> getComments() {
-		return comments;
-	}
-
-	public void setComments(List<CommentDTO> comments) {
-		this.comments = comments;
-	}
-		
 }
